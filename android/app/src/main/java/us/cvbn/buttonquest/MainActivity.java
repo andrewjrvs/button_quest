ee9a2afaea3665240d7d5f36e11f404ae0a5e9a0
Andrew Jarvis <6360004+andrewjrvs@users.noreply.github.com>
2022-08-02 02:57:59 +0000

    framework

---
+++ android/app/src/main/java/us/cvbn/buttonquest/MainActivity.java
@@ -0,0 +1,5 @@
+package us.cvbn.buttonquest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'us.cvbn.buttonquest',
+  appName: 'button-quest',
+  webDir: 'dist/button-quest',
+  bundledWebRuntime: false
+};
+
+export default config;
